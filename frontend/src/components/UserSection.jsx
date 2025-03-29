90d3241180e37992576e86e00c823a7a3d5d6013
Antonio Mas <antoniop2004@gmail.com>
2025-03-29 16:33:22 +0100

    Frontend Parte 23: Carrusel Usuarios

--- frontend/src/components/UserSection.jsx
+++ frontend/src/components/UserSection.jsx
@@ -1,18 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { FaUser } from "react-icons/fa"; // Importa los íconos de estrella y usuario
+import React, { useEffect, useState } from "react";
+import { FaUser } from "react-icons/fa";
 import axios from "axios";
-import UserCard from "./UserCard"; // Importar el componente UserCard
-import "../styles/UserSection.css"; // Asegúrate de tener los estilos adecuados
+import UserCarousel from "./UserCarousel";
+import "../styles/UserSection.css";
 
 const UserSection = () => {
   const [usuarios, setUsuarios] = useState([]);
 
-  // Llamada a la API para obtener todos los usuarios
   useEffect(() => {
     const fetchUsuarios = async () => {
       try {
         const response = await axios.get("http://localhost:5000/api/usuarios/");
-        setUsuarios(response.data); // Guardamos los usuarios obtenidos en el estado
+        setUsuarios(response.data);
       } catch (error) {
         console.error("Error al obtener usuarios:", error);
       }
@@ -22,20 +21,11 @@ const UserSection = () => {
   }, []);
 
   return (
-    <div className="user-section">
-      <h2>
-        <span className="user-icon"><FaUser /></span> Creadores Destacados
-      </h2>
-      <div className="user-section-cards">
-        {usuarios.map((usuario) => (
-          <UserCard
-            key={usuario._id}
-            nickname={usuario.nickname}
-            fotoPerfil={usuario.fotoPerfil}
-          />
-        ))}
-      </div>
-    </div>
+    <UserCarousel
+      title="Creadores Destacados"
+      icon={<FaUser />}
+      users={usuarios}
+    />
   );
 };
 
