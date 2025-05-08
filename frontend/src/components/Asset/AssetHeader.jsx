fbd7940f93c031733d5e312e630bc98750760a39
Antonio Mas <antoniop2004@gmail.com>
2025-05-08 12:47:06 +0200

    Enlace Perfil Corregido

--- frontend/src/components/Asset/AssetHeader.jsx
+++ frontend/src/components/Asset/AssetHeader.jsx
@@ -1,12 +1,28 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import styles from "./AssetHeader.module.css";
 import FavoritoButton from "./FavoritoButton";
+import { getUsuarioPorNickname } from "@/services/userService";
 
 const AssetHeader = ({ asset }) => {
   if (!asset) return null;
 
+  const [emailAutor, setEmailAutor] = useState(null);
+
   const { titulo, autor, fechaCreacion, vistas, disponible, _id } = asset;
 
+  useEffect(() => {
+    const fetchEmailAutor = async () => {
+      try {
+        const datosAutor = await getUsuarioPorNickname(autor);
+        setEmailAutor(datosAutor.email);
+      } catch (error) {
+        console.error("Error obteniendo el email del autor:", error);
+      }
+    };
+
+    fetchEmailAutor();
+  }, [autor]);
   const fechaFormateada = new Date(fechaCreacion).toLocaleDateString("es-ES", {
     year: "numeric",
     month: "long",
@@ -23,8 +39,8 @@ const AssetHeader = ({ asset }) => {
       <div className={styles.metaInfo}>
         <span className={styles.author}>
           Creado por{" "}
-          <Link to={`/user/${autor}`} className={styles.authorLink}>
-            {autor}
+          <Link to={`/user/${encodeURIComponent(emailAutor)}`} className={styles.authorLink}>
+          {autor}
           </Link>
         </span>
 
